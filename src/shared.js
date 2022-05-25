f455ad8c458418fd3718554b6ee11ea0fb33b056
Modestas Rukšnaitis <modestas+github@ruksnaitis.com>
2022-05-25 22:08:22 +0100

    new feature: input changes applied instantly to current tab

---
+++ src/shared.js
@@ -0,0 +1,69 @@
+const defaultConf = {
+    textColor: 'red',
+    proportion: 0.5,
+    blacklist: []
+}
+
+function withProp(key, func) {
+    chrome.storage.sync.get(key, storedConf => {
+        const computedValue = storedConf[key] || defaultConf[key];
+
+        console.log(`${key} := ${computedValue}`);
+
+        func(computedValue);
+    });
+}
+
+const BIONIC_WORD = "bionic-word";
+const BIONIC = "bionic";
+
+function addBionicMarkup() {
+    withProp('proportion', proportion => {
+        function makeBionic(word) {
+            const bionicWord = document.createElement('span');
+            bionicWord.className = BIONIC_WORD;
+
+            const bionicPart = document.createElement('b');
+            bionicPart.className = BIONIC;
+
+            const splitIndex = Math.round(0.5 + proportion * (word.length - 1));
+            bionicPart.textContent = word.substring(0, splitIndex);
+            const restOfWord = document.createTextNode(word.substring(splitIndex) + ' ');
+
+            bionicWord.appendChild(bionicPart);
+            bionicWord.appendChild(restOfWord);
+
+            return bionicWord;
+        }
+
+        function addBionicMarkupTo(node) {
+            switch (node.nodeType) {
+                case Node.ELEMENT_NODE:
+                    if (node.className == BIONIC_WORD)
+                        node.replaceWith(makeBionic(node.textContent.trim()));
+                    else
+                        [...node.childNodes].forEach(addBionicMarkupTo);
+                    break;
+                case Node.TEXT_NODE:
+                    const words = node.textContent.trim().split(' ');
+                    const bionicWords = words.map(makeBionic);
+
+                    node.replaceWith(...bionicWords);
+                    break;
+            }
+        }
+
+        const paragraphs = document.getElementsByTagName('p');
+        [...paragraphs].forEach(addBionicMarkupTo);
+    });
+}
+
+function addBionicStyles() {
+    withProp('textColor', textColor => {
+        const bionicParts = document.getElementsByClassName(BIONIC);
+
+        [...bionicParts].forEach(bionicPart => {
+            bionicPart.style.color = textColor;
+        });
+    });
+}

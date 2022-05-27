c387ca0835ae831688068ba02c2dbebf70cf178f
Ben McAlindin <bmcalindin@gmail.com>
2022-05-27 12:21:20 +0200

    Use mutation observer to check for changes in document

--- src/content.js
+++ src/content.js
@@ -1,11 +1,40 @@
+const targetNode = document.body;
+const config = { attributes: true, childList: true, subtree: true };
+
+const callback = function(mutationsList, observer) {
+    const hasMutated = mutationsList.some(m => {
+        const classes = [
+            ...Array.from(m.addedNodes || []),
+            ...Array.from(m.removedNodes || []),
+        ].reduce((acc, cur) => [...acc, ...Array.from(cur.classList || [])], []);
+        if (classes.includes(BIONIC_WORD) || classes.includes(BIONIC)) return false;
+        return true;
+    });
+    if(hasMutated) {
+        runAfterMutate();
+    }
+}
+
+const observer = new MutationObserver(callback);
+
+function observe() {
+    observer.observe(targetNode, config);
+}
+
+function runAfterMutate() {
+    observer.disconnect();
+    run();
+}
+
+
 function run() {
     let computedValue;
-    chrome.storage.sync.get('blacklist', storedConf => {
-        computedValue = storedConf['blacklist'] || [];
+    chrome.storage.sync.get('blacklist', blacklisted => {
+        computedValue = blacklisted['blacklist'] || [];
         if(!computedValue.includes(window.location.hostname)) {
-            addBionicMarkup();
-            addBionicStyles();
+            addBionicMarkup(addBionicStyles(observe));
         }
     });
 }
-run();
+
+observe();
